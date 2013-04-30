b660d215436b152af6decb0eb033ada79d85deed
unknown <daxin2108@gmail.com>
2013-04-30 00:47:14 -0500

    final commit

---
+++ finalproject/489Project.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Players - Google Fusion Tables</title>
+<style type="text/css">
+html, body, #googft-mapCanvas {
+height: 720px;
+margin: 0;
+padding: 0;
+width: 1080px;
+}
+#googft-legend{background-color:#fff;border:1px solid #000;font-family:Arial, sans-serif;font-size:12px;margin:5px;padding:10px 10px 8px;}#googft-legend p{font-weight:bold;margin-top:0;}#googft-legend div{margin-bottom:5px;}.googft-legend-swatch{border:1px solid;float:left;height:12px;margin-right:8px;width:20px;}.googft-legend-range{margin-left:0;}.googft-dot-icon{margin-right:8px;}.googft-paddle-icon{height:24px;left:-8px;margin-right:-8px;position:relative;vertical-align:middle;width:24px;}.googft-legend-source{margin-bottom:0;margin-top:8px;}.googft-legend-source a{color:#666;font-size:11px;}
+</style>
+
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+
+<script type="text/javascript">
+function initialize() {
+map = new google.maps.Map(document.getElementById('googft-mapCanvas'), {
+center: new google.maps.LatLng(33.204986343555134, -84.91630149786398),
+zoom: 3,
+mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+layer = new google.maps.FusionTablesLayer({
+map: map,
+heatmap: { enabled: false },
+query: {
+select: "col8",
+from: "1RgRAIbRB5U5wfkB4oDnc8hhk0f556dbOwFbMrNg",
+where: ""
+},
+options: {
+styleId: 2,
+templateId: 2
+}
+});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Players - Google Fusion Tables</title>
+<style type="text/css">
+html, body, .googft-mapCanvas {
+height: 720px;
+margin: 0 auto;
+padding: 0;
+width: 1080px;
+}
+#googft-legend{background-color:#fff;border:1px solid #000;font-family:Arial, sans-serif;font-size:12px;margin:5px;padding:10px 10px 8px;}#googft-legend p{font-weight:bold;margin-top:0;}#googft-legend div{margin-bottom:5px;}.googft-legend-swatch{border:1px solid;float:left;height:12px;margin-right:8px;width:20px;}.googft-legend-range{margin-left:0;}.googft-dot-icon{margin-right:8px;}.googft-paddle-icon{height:24px;left:-8px;margin-right:-8px;position:relative;vertical-align:middle;width:24px;}.googft-legend-source{margin-bottom:0;margin-top:8px;}.googft-legend-source a{color:#666;font-size:11px;}
+</style>
+
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+
+<script type="text/javascript">
+function initialize() {
+map = new google.maps.Map(document.getElementById('googft-mapCanvas2'), {
+center: new google.maps.LatLng(41.4042806130943, -88.95927024786398),
+zoom: 4,
+mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+layer = new google.maps.FusionTablesLayer({
+map: map,
+heatmap: { enabled: false },
+query: {
+select: "col8",
+from: "1RgRAIbRB5U5wfkB4oDnc8hhk0f556dbOwFbMrNg",
+where: "col0 contains ignoring case \x27stanford\x27"
+},
+options: {
+styleId: 2,
+templateId: 2
+}
+});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+
+<script type="text/javascript">
+function initialize() {
+map = new google.maps.Map(document.getElementById('googft-mapCanvas3'), {
+center: new google.maps.LatLng(41.4042806130943, -88.95927024786397),
+zoom: 4,
+mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+layer = new google.maps.FusionTablesLayer({
+map: map,
+heatmap: { enabled: false },
+query: {
+select: "col8",
+from: "1RgRAIbRB5U5wfkB4oDnc8hhk0f556dbOwFbMrNg",
+where: "col0 in (\x27Oklahoma\x27, \x27Oklahoma St.\x27)"
+},
+options: {
+styleId: 2,
+templateId: 2
+}
+});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+
+<script type="text/javascript">
+function initialize() {
+map = new google.maps.Map(document.getElementById('googft-mapCanvas4'), {
+center: new google.maps.LatLng(34.04658889067626, -87.37723899786398),
+zoom: 4,
+mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+layer = new google.maps.FusionTablesLayer({
+map: map,
+heatmap: { enabled: false },
+query: {
+select: "col8",
+from: "1RgRAIbRB5U5wfkB4oDnc8hhk0f556dbOwFbMrNg",
+where: "col10 \x3c\x3d \x272013\x27 and col10 \x3e\x3d \x272004\x27 and col3 \x3d 6.1"
+},
+options: {
+styleId: 2,
+templateId: 2
+}
+});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+</head>
+
+<body>
+<div style="background-color:#ddd">
+
+
+<a href="https://www.google.com/fusiontables/DataSource?docid=1RgRAIbRB5U5wfkB4oDnc8hhk0f556dbOwFbMrNg#map:id=3" target="_blank"><img src="johnny.jpg"></a>
+<div class="googft-mapCanvas" id="googft-mapCanvas"></div>
+<p>
+Since the class of 2002<br/>
+-117 Schools have Texas kids<br/>
+-102 in the last 5 years<br/>
+-91 in the last 3<br/>
+-70 last year<br/>
+-Baylor-1<br/>
+-Houston-2<br/>
+-Texas A&M-3 <br/>
+-Texas-4<br/>
+-TCU-5<br/>
+-Tech-6<br/>
+-Rice-7<br/>
+-Oklahoma State-8<br/>
+-OU-11<br/>
+-Tulsa-12<br/>
+-Mizzou-13<br/>
+-Kansas-14<br/>
+-Louisiana-Monroe -15<br/>
+</p>
+<p>
+Last 10 years of the 33 5 stars from Texas <br/>
+-12 to Texas<br/>
+-8 to OU<br/>
+-3 to LSU and A&M<br/>
+</p>
+<p>
+Last 10 years of the 34 5 stars from Cali<br/>
+-18 went to USC<br/>
+-Others were spread with the most having 2<br/>
+</p>
+<p>
+Since 2002 Stanford has<br/>
+-Recruited 222 kids from 33 different states plus DC and Canada<br/>
+-55 from Cali and 23 from Texas (second)<br/>
+</p>
+<div class="googft-mapCanvas" id="googft-mapCanvas2"></div>
+<p>
+Height/weight<br/>
+-The tallest recruit was measured at 6 foot 11 inches, Robert nelson Offensive linemen out of Scottsdale Arizona CC weighed 350 pounds.<br/>
+-5 players measured in at 5 foot 5<br/>
+-2 players weighed 139 pounds, both ran 4.4 40s<br/>
+-The biggest weighed 395, Terrence cody at Alabama (he turned into a 2nd round draft pick) and is now 350<br/>
+</p>
+<p>
+From 2004-2007 Alabama signed 1 5 star player<br/>
+2004 6-6<br/>
+2005 0-2 with 10 wins vacated<br/>
+2006 0-7 with 6 wins vacated<br/>
+2007 2-6 with 5 wins vacated (Saban's First year)<br/>
+From 2008-2013 they’ve signed 18<br/>
+12-2<br/>
+14-0<br/>
+10-3<br/>
+12-1<br/>
+13-1 (loss to Texas A&M)<br/>
+</p>
+<p>
+Since 2004 there have been 305 5 star recruits - 39.3 % have come from Florida, California, and Texas in the order (120)<br/>
+Only 34 states have had at least 1 5-star<br/>
+</p>
+<div class="googft-mapCanvas" id="googft-mapCanvas4"></div>
+<p>
+232 recruits were reported as having ran under a 4.4 40, 72 were reported in just 2002<br/>
+Out of 160 between 2003-2013, 10% chose either florida state or florida (9-FSU 7-UF)<br/>
+</p>
+<p>
+36.1% (91/252) of 5 star recruits in the last 8 classes chose SEC schools. Florida won the NC in 2006<br/>
+</p>
+<p>
+Oregon and Oregon state has (152/281) 54% and (155/294) 52% kids from California yet they only have (26/281) 9.25% and (33/294) 11.22% from their home state.<br/>
+</p>
+<p>
+Oklahoma and Oklahoma state has 44.64% (125/280) and 59% (180/305) from Texas<br/>
+But only OU has 21.8% (61/280) and has OSU 17% (52/305) from Oklahoma<br/>
+</p>
+<div class="googft-mapCanvas" id="googft-mapCanvas3"></div>
+<p>
+27.5% (73/265) of recruits that signed with Michigan are from Michigan and 21.1% (58/265) are from Ohio<br/>
+Ohio State has 58.5% (148/253) from Ohio and 2.3% (6/253) from Michigan<br/>
+</p>
+<p>
+Over  60% (56/93) of kids playing wr rb db or ath that signed with Oregon clocked in the forty faster than a 4.5<br/>
+</p>
+<p>
+SEC states including Missouri and Texas account for 14453/31917 recruits<br/>
+From 2006 – 2011 82 elite recruits from a SEC state (excluding Texas and Missouri) chose an SEC school which comprises of 12 schools, 52 chose the field<br/>
+</p>
+</div>
+</body>
+</html>
